e2a638c9f826296ac8704e146980fe1f313ea540
Dmitry Alexandrov <alexandrov.dm91@gmail.com>
2025-02-19 21:10:19 +0300

    lawn farm is done!

--- haddan/haddan_miner/constants.py
+++ haddan/haddan_miner/constants.py
@@ -9,9 +9,11 @@ load_dotenv()
 BASE_DIR = Path(__file__).parent
 DOWNLOADS_DIR_NAME = 'temp'
 KAPCHA_NAME = 'kapcha.png'
+SCREENSHOT_NAME = 'screenshot.png'
 PAGE_SOURCE_NAME = 'page_source.html'
 KAPCHA_PATH = BASE_DIR / DOWNLOADS_DIR_NAME / KAPCHA_NAME
 PAGE_SOURCE_PATH = BASE_DIR / DOWNLOADS_DIR_NAME / PAGE_SOURCE_NAME
+SCREENSHOT_PATH = BASE_DIR / DOWNLOADS_DIR_NAME / SCREENSHOT_NAME
 
 # константы адресов.
 HADDAN_MAIN_URL = 'https://haddan.ru/'
@@ -20,10 +22,22 @@ MEDITATION_URL = 'https://haddan.ru/room/func/temple.php'
 KAPCHA_URL = 'https://haddan.ru/inner/img/gc.php'
 
 # Переменные окружения.
-USERNAME = os.getenv('HADDAN_USERNAME')
+FIRST_CHAR = os.getenv('FIRST_CHAR')
+SECOND_CHAR = os.getenv('SECOND_CHAR')
 PASSWORD = os.getenv('HADDAN_PASSWORD')
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 # Остальное.
 PAUSE_DURATION_SECONDS = 50
+
+# Цена ресурсов поляны
+FIELD_PRICES = {
+    'Мухожор': 9,
+    'Подсолнух': 17,
+    'Капустница': 28,
+    'Мандрагора': 40,
+    'Зеленая массивка': 67,
+    'Колючник Черный': 101,
+    'Гертаниум': 210
+    }
--- haddan/haddan_miner/utils.py
+++ haddan/haddan_miner/utils.py
@@ -1,7 +1,11 @@
 """Утилитки приложения haddan_miner"""
 from time import sleep
-from constants import PAGE_SOURCE_PATH, PASSWORD, TELEGRAM_CHAT_ID, USERNAME
+import re
+
 from selenium.webdriver.common.by import By
+
+from constants import (FIELD_PRICES, FIRST_CHAR, HADDAN_MAIN_URL, PAGE_SOURCE_PATH, PASSWORD,
+                       SECOND_CHAR, TELEGRAM_CHAT_ID)
 
 
 def send_photo(bot, photo):
@@ -16,11 +20,27 @@ def save_url_content(driver):
         file.write(page_source)
 
 
-def login_to_haddan(driver):
+def first_char_login(driver):
     """Вход в игру."""
     username_field = driver.find_element(
         By.NAME, 'username')
-    username_field.send_keys(USERNAME)
+    username_field.send_keys(FIRST_CHAR)
+    sleep(1)
+    password_field = driver.find_element(
+        By.NAME, 'passwd')
+    password_field.send_keys(PASSWORD)
+    sleep(1)
+    submit_button = driver.find_element(
+        By.CSS_SELECTOR,
+        '[href="javascript:enterHaddan()"]')
+    submit_button.click()
+
+
+def second_char_login(driver):
+    """Вход в игру."""
+    username_field = driver.find_element(
+        By.NAME, 'username')
+    username_field.send_keys(SECOND_CHAR)
     sleep(1)
     password_field = driver.find_element(
         By.NAME, 'passwd')
@@ -34,7 +54,7 @@ def login_to_haddan(driver):
 
 def get_temple_kaptcha(driver):
     """Возвращает картинку  капчей из храма."""
-    # Переключаемся в комнату медатации.
+    # Переключаемся в комнату копки.
     driver.switch_to.frame("frmcenterandchat")
     driver.switch_to.frame("frmcentral")
     # Нажимаем кнопку медитация.
@@ -43,8 +63,145 @@ def get_temple_kaptcha(driver):
     sleep(1)
     # Переключаемся на фрейм с медитацией
     driver.switch_to.frame("func")
-    # Находим экапчу и отправляем в телеграм
+    # Находим капчу и отправляем в телеграм
+    image_element = driver.find_element(
+        By.CSS_SELECTOR,
+        'img[src="/inner/img/gc.php"]')
+    if image_element:
+        return image_element
+
+
+def get_mine_kaptcha(driver):
+    """Возвращает картинку  капчей из храма."""
+    # Переключаемся в комнату медатации.
+    driver.switch_to.frame("frmcenterandchat")
+    driver.switch_to.frame("frmcentral")
+    # Нажимаем кнопку Копать.
+    mine_link = driver.find_element(By.LINK_TEXT, "Копать")
+    mine_link.click()
+    sleep(1)
+    # Переключаемся на фрейм с копкой
+    driver.switch_to.frame("func")
+    # Находим капчу и отправляем в телеграм
+    image_element = driver.find_element(
+        By.CSS_SELECTOR,
+        'img[src="/inner/img/gc.php"]')
+    if image_element:
+        return image_element
+
+
+def switch_to_central_frame(driver):
+    """Переключается на центральный фрейм окна."""
+    driver.switch_to.frame("frmcenterandchat")
+    driver.switch_to.frame("frmcentral")
+
+
+def find_kaptcha(driver):
+    driver.switch_to.frame("func")
     image_element = driver.find_element(
         By.CSS_SELECTOR,
         'img[src="/inner/img/gc.php"]')
     return image_element
+
+
+def get_kaptcha_answer(message, driver):
+    """Отправляет ответ на капчу."""
+    button = driver.find_element(
+        By.CSS_SELECTOR,
+        f'button[value="{message}"]')
+    button.click()
+
+
+def price_counter(resurses):
+    """Находит самый дорогой ресурс из списка."""
+    result = []
+    for s in resurses:
+        pattern = r'(\D+)\s+-\s+(\d+)'
+        match = re.match(pattern, s)
+        if match:
+            part1 = match.group(1).strip()
+            part2 = int(match.group(2))
+            result.append(part2 * FIELD_PRICES[f'{part1}'])
+    most_cheep_res = result.index(max(result))
+    return most_cheep_res
+
+
+class HaddanBot():
+
+    """Бот класс управления действий персонажа.
+
+    Принимает два обязательных аргумента при инициализации:
+        char - никнейм персонажа,
+        driver - экземпляр класса  webdriver.Chrome.
+
+    """
+
+    def __init__(self, char, driver):
+        self.driver = driver
+        self.char = char
+        # self.bot = bot
+
+    def login_to_game(self):
+        """Заходит в игру под заданным именем char."""
+        self.driver.get(HADDAN_MAIN_URL)
+        self.driver.maximize_window()
+        sleep(1)
+        username_field = self.driver.find_element(
+            By.NAME, 'username')
+        username_field.send_keys(self.char)
+        sleep(1)
+        password_field = self.driver.find_element(
+            By.NAME, 'passwd')
+        password_field.send_keys(PASSWORD)
+        sleep(1)
+        submit_button = self.driver.find_element(
+            By.CSS_SELECTOR,
+            '[href="javascript:enterHaddan()"]')
+        submit_button.click()
+        sleep(5)
+
+    def save_page_content(self):
+        """Сохраняет HTML код страницы в файл."""
+        page_source = self.driver.page_source
+        with open(PAGE_SOURCE_PATH, "w", encoding="utf-8") as file:
+            file.write(page_source)
+
+    def switch_to_central_frame(self):
+        """Переключается на центральный фрейм окна."""
+        self.driver.switch_to.frame("frmcenterandchat")
+        self.driver.switch_to.frame("frmcentral")
+
+    def click_to_glade_fairy(self):
+        """Начать диалог с феей поляны."""
+        glade_fairy = self.driver.find_element(
+            By.CSS_SELECTOR,
+            'img[id="roomnpc231778"]')
+        glade_fairy.click()
+
+    def start_farm_herbs(self):
+        """Кликает на вариант ответа "Да, мне нужны новые травы"."""
+        self.driver.switch_to.frame("thedialog")
+        battle_start = self.driver.find_element(
+            By.CLASS_NAME,
+            'talksayTak')
+        battle_start.click()
+        if self.driver.find_element(
+            By.CLASS_NAME,
+            'talksayTak'
+        ):
+            confirm = self.driver.find_element(
+                By.CLASS_NAME,
+                'talksayTak')
+            confirm.click()
+
+    def battle(self):
+        """Автобой (тыкает на изображение npc пока бой не закончится)."""
+        while self.driver.find_element(
+                By.PARTIAL_LINK_TEXT, 'Ударить'
+                ):
+            hit = self.driver.find_element(
+                By.CSS_SELECTOR,
+                'img[onclick="touchFight();"]')
+            hit.click()
+            sleep(1)
+
